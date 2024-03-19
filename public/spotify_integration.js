99ab19165c97b4033db8a0c5fd4b063fd390ac9e
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2024-03-19 17:15:49 -0300

    Update spotify_integration.js

--- public/spotify_integration.js
+++ public/spotify_integration.js
@@ -286,7 +286,6 @@ async function fetchCurrentlyPlayingData() {
         if (response.status === 204) {
             document.getElementById('sp_player_div').style.display = ''; // Exibir elemento
             document.getElementById('sp_connect').style.display = 'none'; // Ocultar elemento
-            document.getElementById('sp_connect_icon_green').style.display = 'none'
             document.getElementById('sp_fast_transfer').style.display = 'none'
             document.getElementById('playback_info').style.display = 'none'; // Ocultar elemento
             document.getElementById('no_playback').style.display = 'block'; // Ocultar elemento
