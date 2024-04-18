e5dbc7115b82cd91d53a6eb84f9267bc12b3da5a
Marcos Wiendl <127610129+mwndl@users.noreply.github.com>
2024-04-18 18:30:49 -0300

    2.15.3
    elemento de descrição dos créditos readicionado para edições futuras

--- public/script_formatter.js
+++ public/script_formatter.js
@@ -6,7 +6,7 @@ let undoCursorPositionsStack = [];
 var redoCursorPositionsStack = [];
 var maxStackSize = 100;
 
-var lf_version = '2.15.2';
+var lf_version = '2.15.3';
 var lf_release_date = '18/04/2024'
 
 document.addEventListener('DOMContentLoaded', function () {
